63f9229cd94f540f5ee68d99ed2fc122fde69be4
Pamela Gonzalez Perilli <pamgonzalezperilli@gmail.com>
2021-06-08 09:37:22 -0300

    Includes Pegin Status Service without logic (mock data) and pegin status controller calls to pegin status service

--- src/models/pegin-status.model.ts
+++ src/models/pegin-status.model.ts
@@ -1,44 +1,98 @@
 import {Model, model, property} from '@loopback/repository';
 
 @model()
-export class PeginStatus extends Model {
+export class BtcPeginStatus extends Model {
   @property({
     type: 'string',
   })
-  btcTxId: string;
+  txId: string;
 
   @property({
-    type: 'Status',
+    type: 'date',
+    defaultFn: 'now'
+  })
+  creationDate: Date;
+
+  @property({
+    type: 'string',
+  })
+  federationAddress: string;
+
+  @property({
+    type: 'number',
+  })
+  amountTransferred: number;
+
+  @property({
+    type: 'string',
+  })
+  refundAddress: string;
+
+  @property({
+    type: 'number',
+  })
+  confirmations: number;
+
+  @property({
+    type: 'number',
+  })
+  requiredConfirmation: number;
+
+  constructor(btcTxId: string) {
+    super();
+    this.txId = btcTxId;
+  }
+}
+
+@model()
+export class RskPeginStatus extends Model {
+  @property({
+    type: 'string',
+  })
+  recipientAddress: string;
+
+  @property({
+    type: 'number',
+  })
+  confirmations: number;
+
+  constructor() {
+    super();
+  }
+}
+
+@model()
+export class PeginStatus extends Model {
+  @property({
+    type: 'object',
+    required: true,
+  })
+  btc: BtcPeginStatus;
+
+  @property({
+    type: 'object',
+  })
+  rsk: RskPeginStatus;
+
+  @property({
+    type: 'object',
   })
   status: Status;
 
-  @property({
-    type: 'number',
-  })
-  rskBlockHeight: number;
-
-  @property({
-    type: 'string',
-  })
-  rskTxId: string;
-
-  @property({
-    type: 'string',
-  })
-  rskRecipient?: string;
-
-  @property({
-    type: 'number',
-  })
-  btcConfirmation?: number;
-
-  constructor(txId: string) {
+  constructor(btc: BtcPeginStatus, status: Status, rsk?: RskPeginStatus) {
     super();
-    this.btcTxId = txId;
+    this.status = status;
+    if (rsk) {
+      this.rsk = rsk;
+    } else {
+      this.rsk = new RskPeginStatus();
+    }
+    this.btc = btc;
   }
 }
 
 export enum Status {
+  not_in_btc_yet,
   waiting_confirmations,
   confirmed,
   rejected_no_refund,
