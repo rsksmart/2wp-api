86343066de05d318912a25118ff1367c40142f55
José Dahlquist <jose.dahlquist@gmail.com>
2021-07-19 13:57:22 -0300

    Add data filters to RskBridgeDataProvider
    - create BridgeDataFilter to specify which methods to search
    - configure filter to search for registerBtcTransaction
    - improve syncing logic and logging

---
+++ src/models/bridge-data-filter.model.ts
@@ -0,0 +1,12 @@
+export class BridgeDataFilterModel {
+  abiEncodedSignature: string;
+
+  constructor(abiEncodedSignature: string) {
+    this.abiEncodedSignature = abiEncodedSignature;
+  }
+
+  isMethodCall(callData: string) {
+    return callData.startsWith(this.abiEncodedSignature) ||
+      callData.startsWith('0x' + this.abiEncodedSignature);
+  }
+}
--- src/services/node-bridge-data.provider.ts
+++ src/services/node-bridge-data.provider.ts
@@ -1,4 +1,5 @@
 import {getLogger, Logger} from 'log4js';
+import {BridgeDataFilterModel} from '../models/bridge-data-filter.model';
 import {BridgeData} from '../models/rsk/bridge-data.model';
 import {Log} from '../models/rsk/log.model';
 import {Transaction} from '../models/rsk/transaction.model';
@@ -8,13 +9,15 @@ import {RskNodeService} from './rsk-node.service';
 export class NodeBridgeDataProvider implements RskBridgeDataProvider {
   rskNodeService: RskNodeService;
   logger: Logger;
+  filters: Array<BridgeDataFilterModel>;
   constructor() {
     this.rskNodeService = new RskNodeService();
+    this.filters = [];
     this.logger = getLogger('nodeBridgeDataProvider');
   }
   async getData(startingBlock: string | number): Promise<BridgeData> {
     let data: BridgeData = new BridgeData();
-    this.logger.trace(`Fetching data for block ${startingBlock}`);
+    // this.logger.trace(`Fetching data for block ${startingBlock}`);
     let lastBlock = await this.rskNodeService.getBlock(startingBlock);
     if (lastBlock == null) {
       throw new Error(`Block ${startingBlock} doesn't exist`);
@@ -25,18 +28,30 @@ export class NodeBridgeDataProvider implements RskBridgeDataProvider {
       if (transaction.to !== '0x0000000000000000000000000000000001000006') {
         continue;
       }
-      this.logger.trace(`Found a bridge tx ${transaction.hash}`);
-      let tx = new Transaction();
-      tx.blockHeight = lastBlock.number;
-      tx.blockHash = lastBlock.hash;
-      tx.hash = transaction.hash;
-      tx.data = transaction.input;
-      let txReceipt = await this.rskNodeService.getTransactionReceipt(tx.hash.toString('hex'));
-      tx.logs = <Array<Log>>txReceipt.logs;
-      data.data.push(tx);
+
+      this.logger.trace(`Found a bridge tx ${transaction.hash} with signature ${transaction.input.substring(0, 10)}`);
+
+      if (this.filters.length == 0 ||
+        this.filters.some(f => f.isMethodCall(transaction.input))
+      ) {
+        this.logger.debug(`Tx ${transaction.hash} matches filters`);
+
+        let tx = new Transaction();
+        tx.blockHeight = lastBlock.number;
+        tx.blockHash = lastBlock.hash;
+        tx.hash = transaction.hash;
+        tx.data = transaction.input;
+        let txReceipt = await this.rskNodeService.getTransactionReceipt(tx.hash.toString('hex'));
+        tx.logs = <Array<Log>>txReceipt.logs;
+        data.data.push(tx);
+      }
     }
 
     return Promise.resolve(data);
+  }
+
+  configure(filters: Array<BridgeDataFilterModel>): void {
+    this.filters = filters || [];
   }
 
 }
