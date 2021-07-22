156a17f2a57a56df2df43ba5f89b786e43482b10
José Dahlquist <jose.dahlquist@gmail.com>
2021-07-22 17:48:57 -0300

    Add optional appmode argument
    argument => `--appmode=`
    values:
    - ALL: default value. starts API and daemon+
    - API: only starts API
    - DAEMON: only starts daemon
    
    Include new npm scripts to easily start both modes:
    - start-api: builds and executes passing api only argument
    - start-daemon: builds and executes passing daemon only argument

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,44 @@
+import {config} from 'dotenv';
 import {configure, getLogger} from 'log4js';
 import {ApplicationConfig, TwpapiApplication} from './application';
 import {DaemonRunner} from './daemon-runner';
 
 export * from './application';
 
-export async function main(options: ApplicationConfig = {}): Promise<TwpapiApplication> {
+enum APP_MODE {
+  API,
+  DAEMON,
+  ALL
+};
+
+const searchAppMode = (): APP_MODE => {
+  let arg = process.argv.find(a => a.startsWith('--appmode='));
+  if (arg) {
+    let value: string = arg.split('=')[1];
+    let parsedEnum = APP_MODE[value as keyof typeof APP_MODE];
+    return parsedEnum !== undefined ? parsedEnum : APP_MODE.ALL;
+  }
+  return APP_MODE.ALL;
+};
+
+export async function main(options: ApplicationConfig = {}): Promise<void> {
   configure('./log-config.json');
 
   let logger = getLogger('app');
+
+  let api: TwpapiApplication;
+  let daemon: DaemonRunner;
 
   let shuttingDown = false;
   async function shutdown() {
     if (!shuttingDown) {
       shuttingDown = true;
-      await app.stop();
-      await daemon.stop();
+      if (api) {
+        await api.stop();
+      }
+      if (daemon) {
+        await daemon.stop();
+      }
       logger.info('Shutting down');
     }
   }
@@ -25,17 +49,21 @@ export async function main(options: ApplicationConfig = {}): Promise<TwpapiAppli
   //catches uncaught exceptions
   process.on('uncaughtException', shutdown.bind(null));
 
-  const app = new TwpapiApplication(options);
-  await app.boot();
-  await app.start();
+  let appMode = searchAppMode();
 
-  const url = app.restServer.url;
-  logger.info(`Server is running at ${url}`);
+  config();
+  if (appMode == APP_MODE.API || appMode == APP_MODE.ALL) {
+    api = new TwpapiApplication(options);
+    await api.boot();
+    await api.start();
 
-  let daemon: DaemonRunner = new DaemonRunner();
-  await daemon.start();
-
-  return app;
+    const url = api.restServer.url;
+    logger.info(`Server is running at ${url}`);
+  }
+  if (appMode == APP_MODE.DAEMON || appMode == APP_MODE.ALL) {
+    daemon = new DaemonRunner();
+    await daemon.start();
+  }
 }
 
 if (require.main === module) {
