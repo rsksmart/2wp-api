6dfaec1288ecf4febd4cafe27fb5ab5c21023b67
cazdemun <cplu1509@gmail.com>
2021-08-02 10:06:34 -0500

    cover repository lines

--- src/__tests__/unit/session.repository.unit.ts
+++ src/__tests__/unit/session.repository.unit.ts
@@ -8,9 +8,10 @@ import {TxFeeController} from '../../controllers';
 import {FeeLevel} from '../../services';
 import {sinon} from '@loopback/testlab/dist/sinon';
 import {getMockInputs, getUtxoList} from '../helper';
-import {FeeRequestData} from '../../models';
+import {FeeAmountData, FeeRequestData, Session} from '../../models';
 import * as constants from '../../constants';
 import {config} from 'dotenv';
+import {RedisDataSource} from '../../datasources';
 
 config();
 
@@ -57,5 +58,81 @@ describe('Session Repository', () => {
     expect(feeAmount.slow).to.eql(338);
     expect(feeAmount.average).to.eql(676);
     expect(feeAmount.fast).to.eql(1014);
+  });
+
+  it('should return the appropriate fee level', async () => {
+    const sessionId = 'test_id';
+    const dummySessionRepository = new SessionRepository(new RedisDataSource());
+    const getStub = sinon.stub(dummySessionRepository, 'get');
+
+    getStub.withArgs(sessionId).resolves(
+      new Session({
+        _id: 'test_id',
+        balance: 6,
+        fees: new FeeAmountData({slow: 1, average: 2, fast: 3}),
+      }),
+    );
+
+    const slowFeeLevel = await dummySessionRepository.getFeeLevel(
+      sessionId,
+      constants.BITCOIN_SLOW_FEE_LEVEL,
+    );
+    const averageFeeLevel = await dummySessionRepository.getFeeLevel(
+      sessionId,
+      constants.BITCOIN_AVERAGE_FEE_LEVEL,
+    );
+    const fastFeeLevel = await dummySessionRepository.getFeeLevel(
+      sessionId,
+      constants.BITCOIN_FAST_FEE_LEVEL,
+    );
+
+    expect(slowFeeLevel).to.eql(1);
+    expect(averageFeeLevel).to.eql(2);
+    expect(fastFeeLevel).to.eql(3);
+
+    try {
+      await dummySessionRepository.getFeeLevel(sessionId, 'WRONG_FEE_LEVEL');
+    } catch (e) {
+      expect(e).to.eql(new Error(`Wrong Fee Level: ${'WRONG_FEE_LEVEL'}`));
+    }
+
+    try {
+      getStub.withArgs(sessionId).resolves(
+        new Session({
+          _id: 'test_id',
+          balance: 6,
+          fees: undefined,
+        }),
+      );
+
+      await dummySessionRepository.getFeeLevel(
+        sessionId,
+        constants.BITCOIN_SLOW_FEE_LEVEL,
+      );
+    } catch (e) {
+      expect(e).to.eql(
+        new Error(`There is not fee amount stored for sessionId ${sessionId}`),
+      );
+    }
+  });
+
+  it('should return an empty array for undefined inputs', async () => {
+    const sessionId = 'test_id';
+    const dummySessionRepository = new SessionRepository(new RedisDataSource());
+    const getStub = sinon.stub(dummySessionRepository, 'get');
+
+    getStub.withArgs(sessionId).resolves(
+      new Session({
+        _id: 'test_id',
+        balance: 6,
+        fees: new FeeAmountData({slow: 1, average: 2, fast: 3}),
+        inputs: undefined,
+      }),
+    );
+
+    const dummyInputs = await dummySessionRepository.getAccountInputs(
+      sessionId,
+    );
+    expect(dummyInputs.length).to.eql(0);
   });
 });
