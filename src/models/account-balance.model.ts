cabd1eb4579ae246ad58779c1e0b66d40e0db1c2
Ronald Sarmiento <roasarmientoga@unal.edu.co>
2021-11-22 14:09:05 -0500

    Change Address check on balance Controller
                    I've changed the balance controller verifications for
                    addresses in order to properly check btc network
                    validity and address typ classification.

--- src/models/account-balance.model.ts
+++ src/models/account-balance.model.ts
@@ -27,43 +27,25 @@ export class AccountBalance extends Model {
   }
 
   public calculateWalletBalance(addressBalances: AddressBalance[]): void {
-    const [legacyTestReg, segwitTestReg, nativeTestReg] = [
-      /^[mn][1-9A-HJ-NP-Za-km-z]{26,35}/,
-      /^[2][1-9A-HJ-NP-Za-km-z]{26,35}/,
-      /^[tb][0-9A-HJ-NP-Za-z]{26,41}/,
-    ];
     addressBalances.forEach(addressBalance => {
       const utxoSatoshis = addressBalance.utxoList?.map(utxo => utxo.satoshis);
       if (utxoSatoshis && utxoSatoshis.length > 0) {
         const addressAmount: number = utxoSatoshis.reduce(
           (accumulator, satoshis) => satoshis + accumulator,
         );
-        if (legacyTestReg.test(addressBalance.address))
-          this.legacy += addressAmount;
-        else if (segwitTestReg.test(addressBalance.address))
-          this.segwit += addressAmount;
-        else if (nativeTestReg.test(addressBalance.address))
-          this.nativeSegwit += addressAmount;
+        switch (addressBalance.addressType) {
+          case constants.BITCOIN_LEGACY_ADDRESS:
+            this.legacy += addressAmount;
+            break;
+          case constants.BITCOIN_SEGWIT_ADDRESS:
+            this.segwit += addressAmount;
+            break;
+          case constants.BITCOIN_NATIVE_SEGWIT_ADDRESS:
+            this.nativeSegwit += addressAmount;
+            break;
+        }
       }
     });
-  }
-
-  static getAccountType(address: string): string {
-    const [legacyTestReg, segwitTestReg, nativeTestReg] = [
-      new RegExp(
-        process.env.LEGACY_REGEX ?? /^[mn][1-9A-HJ-NP-Za-km-z]{26,35}/,
-      ),
-      new RegExp(process.env.SEGWIT_REGEX ?? /^[2][1-9A-HJ-NP-Za-km-z]{26,35}/),
-      new RegExp(
-        process.env.NATIVE_SEGWIT_REGEX ?? /^[tb][0-9A-HJ-NP-Za-z]{26,41}/,
-      ),
-    ];
-    if (legacyTestReg.test(address)) return constants.BITCOIN_LEGACY_ADDRESS;
-    else if (segwitTestReg.test(address))
-      return constants.BITCOIN_SEGWIT_ADDRESS;
-    else if (nativeTestReg.test(address))
-      return constants.BITCOIN_NATIVE_SEGWIT_ADDRESS;
-    else return constants.BITCOIN_MULTISIGNATURE_ADDRESS;
   }
 }
 
