70948cc0504c55dfcb454b1ef356ea9418240360
José Dahlquist <jose.dahlquist@gmail.com>
2022-02-03 18:10:14 -0300

    Add troubleshooting logs

--- src/controllers/pegin-configuration.controller.ts
+++ src/controllers/pegin-configuration.controller.ts
@@ -1,17 +1,22 @@
 import {repository} from '@loopback/repository';
 import {get, getModelSchemaRef} from '@loopback/rest';
 import crypto from 'crypto';
+import {getLogger, Logger} from 'log4js';
 import {PeginConfiguration, Session} from '../models';
 import {PeginConfigurationRepository, SessionRepository} from '../repositories';
 import {BridgeService} from '../services';
 
 export class PeginConfigurationController {
+  logger: Logger;
+
   constructor(
     @repository(PeginConfigurationRepository)
     public peginConfigurationRepository: PeginConfigurationRepository,
     @repository(SessionRepository)
     public sessionRepository: SessionRepository,
-  ) { }
+  ) {
+    this.logger = getLogger('pegin-configuration-controller');
+  }
 
   @get('/pegin-configuration', {
     responses: {
@@ -31,16 +36,19 @@ export class PeginConfigurationController {
     },
   })
   async get(): Promise<PeginConfiguration> {
-    const session = {
-      _id: crypto.randomBytes(16).toString('hex'),
-      balance: 0,
-    };
-    const ttlSessionDBExpire =
-      process.env.TTL_SESSIONDB_EXPIRE_MILLISECONDS ?? 3600000;
-    await this.sessionRepository.set(session._id, new Session(session));
-    await this.sessionRepository.expire(session._id, +ttlSessionDBExpire);
-    const bridgeService = new BridgeService();
-    return new Promise<PeginConfiguration>((resolve, reject) => {
+    this.logger.debug('[get] started');
+    return new Promise<PeginConfiguration>(async (resolve, reject) => {
+      const session = {
+        _id: crypto.randomBytes(16).toString('hex'),
+        balance: 0,
+      };
+      const ttlSessionDBExpire =
+        process.env.TTL_SESSIONDB_EXPIRE_MILLISECONDS ?? 3600000;
+      await this.sessionRepository.set(session._id, new Session(session));
+      await this.sessionRepository.expire(session._id, +ttlSessionDBExpire);
+      this.logger.trace(`[get] Got session ${session._id}`);
+      const bridgeService = new BridgeService();
+
       Promise.all([
         bridgeService.getMinPeginValue(),
         bridgeService.getFederationAddress(),
@@ -54,9 +62,13 @@ export class PeginConfigurationController {
             btcConfirmations: Number(process.env.BTC_CONFIRMATIONS) ?? 100,
             sessionId: session._id,
           });
+          this.logger.debug('[get] Finished');
           resolve(peginConf);
         })
-        .catch(reject);
+        .catch((e) => {
+          this.logger.warn(`[get] Got an error. ${e}`);
+          reject(e);
+        });
     });
   }
 }
