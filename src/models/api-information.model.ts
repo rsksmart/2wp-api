e43781b53078b50e5c5d3f99adcc85801b5b5164
Alex Braz <abraz@Alexs-MacBook-Pro.local>
2022-04-08 12:59:15 -0300

    created api-information controllers and the model, and get the version from package.json and showing to the route /api

---
+++ src/models/api-information.model.ts
@@ -0,0 +1,7 @@
+
+export class ApiInformation {
+  version?: string;
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+}
