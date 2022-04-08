ae488bb4e2bdca188cc111d40abb983cd446dd1b
Alex Braz <abraz@Alexs-MacBook-Pro.local>
2022-04-08 15:13:53 -0300

    removed unecessary additional data in the model

--- src/models/api-information.model.ts
+++ src/models/api-information.model.ts
@@ -1,7 +1,4 @@
 
 export class ApiInformation {
   version?: string;
-  // Indexer property to allow additional data
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [prop: string]: any;
 }
