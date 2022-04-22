ef9b1dbdb44f95909a4ff87df0b5d29ec3329e6e
jeremy-then <jeremy.then@iovlabs.org>
2022-04-22 10:11:10 -0400

    Adds  PegoutDataProcessor class

---
+++ src/__tests__/unit/services/pegout-data.processor.unit.ts
@@ -0,0 +1,15 @@
+import {expect} from '@loopback/testlab';
+import { PegoutStatusDataService } from '../../../services/pegout-status-data-services/pegout-status-data.service';
+import { PegoutDataProcessor } from '../../../services/pegout-data.processor';
+
+describe('Service: PegoutDataProcessor', () => {
+
+  it('returns filters', () => {
+    const mockedPegoutStatusDataService = <PegoutStatusDataService>{};
+    const thisService = new PegoutDataProcessor(mockedPegoutStatusDataService);
+    expect(thisService.getFilters()).to.be.Array;
+    expect(thisService.getFilters()).to.not.be.empty;
+    expect(thisService.getFilters().length).to.equal(3);
+  });
+  
+});
---
+++ src/services/pegout-data.processor.ts
@@ -0,0 +1,27 @@
+import {getLogger, Logger} from 'log4js';
+import {RskTransaction} from '../models/rsk/rsk-transaction.model';
+import {BRIDGE_METHODS, getBridgeSignature} from '../utils/bridge-utils';
+import FilteredBridgeTransactionProcessor from '../services/filtered-bridge-transaction-processor';
+import { BridgeDataFilterModel } from '../models/bridge-data-filter.model';
+import { PegoutStatusDataService } from './pegout-status-data-services/pegout-status-data.service';
+
+export class PegoutDataProcessor implements FilteredBridgeTransactionProcessor {
+  logger: Logger;
+  pegoutStatusDataService: PegoutStatusDataService;
+
+  constructor(pegoutStatusDataService: PegoutStatusDataService) {
+    this.logger = getLogger('pegoutDataProcessor');
+    this.pegoutStatusDataService = pegoutStatusDataService;
+  }
+  async process(rskTransaction: RskTransaction): Promise<void> {
+    this.logger.error('[process] Method not yet implemented');
+    throw new Error('Method not yet implemented');
+  }
+  getFilters(): BridgeDataFilterModel[] {
+    return [
+      new BridgeDataFilterModel(getBridgeSignature(BRIDGE_METHODS.UPDATE_COLLECTIONS)),
+      new BridgeDataFilterModel(getBridgeSignature(BRIDGE_METHODS.RELEASE_BTC)),
+      BridgeDataFilterModel.EMPTY_DATA_FILTER
+    ];
+  }
+}
