c2d2de81a988d16b2517c6158000d89abb10274e
Ronald Sarmiento <roasarmientoga@unal.edu.co>
2022-05-18 22:58:35 -0500

    Pegout status service
                    I've created the pegout status service, it fetch the
                    pegout records stored in database in the datamodel
                    format required on the app.

---
+++ src/__tests__/unit/services/pegout-status.service.unit.ts
@@ -0,0 +1,171 @@
+import {createStubInstance, expect, StubbedInstanceWithSinonAccessor} from '@loopback/testlab';
+import sinon from 'sinon';
+import {PegoutStatusService} from "../../../services";
+import {PegoutStatusMongoDbDataService} from "../../../services/pegout-status-data-services/pegout-status-mongo.service";
+import {PegoutStatus, PegoutStatusDataModel} from "../../../models/rsk/pegout-status-data-model";
+
+describe('Pegout Status Service:', () => {
+    let pegoutStatusService: PegoutStatusService;
+    let getLastByOriginatingRskTxHash: sinon.SinonStub;
+    let pegoutStatusDataService: StubbedInstanceWithSinonAccessor<PegoutStatusMongoDbDataService>;
+    beforeEach(() => {
+        pegoutStatusDataService = createStubInstance(PegoutStatusMongoDbDataService);
+        getLastByOriginatingRskTxHash = pegoutStatusDataService.getLastByOriginatingRskTxHash as sinon.SinonStub;
+        pegoutStatusService = new PegoutStatusService(pegoutStatusDataService);
+    });
+    it('should return a valid Pegout Status if there is no record on database', async () => {
+        getLastByOriginatingRskTxHash
+            .withArgs('RskTestTxId')
+            .resolves(null);
+        const pegoutStatus = await pegoutStatusService.getPegoutStatusByRskTxHash('RskTestTxId');
+        const expectedResponse = new PegoutStatusDataModel({
+            status: PegoutStatus.NOT_FOUND,
+        });
+        expect(pegoutStatus).to.be.deepEqual(expectedResponse);
+    });
+    it('should return a Pegout Status: RECEIVED', async () => {
+        getLastByOriginatingRskTxHash
+            .withArgs('RskTestTxId')
+            .resolves({
+                rskTxHash: 'RskTestTxId',
+                rskSenderAddress: 'testSenderAddress',
+                btcRecipientAddress: 'testBtcRecipientAddress',
+                valueInWeisSentToTheBridge: 5000000000000000,
+                valueInSatoshisToBeReceived: 495000,
+                feeInSatoshisToBePaid: 5000,
+                status: PegoutStatus.RECEIVED,
+                btcRawTransaction: 'testBtcRawTx',
+                originatingRskTxHash: 'RskTestTxId',
+                rskBlockHeight: 3158962,
+                lastUpdatedOn: Date.now(),
+            });
+        const pegoutStatus = await pegoutStatusService.getPegoutStatusByRskTxHash('RskTestTxId');
+        const expectedResponse = new PegoutStatusDataModel({
+            rskTxHash: 'RskTestTxId',
+            rskSenderAddress: 'testSenderAddress',
+            btcRecipientAddress: 'testBtcRecipientAddress',
+            valueInWeisSentToTheBridge: 5000000000000000,
+            valueInSatoshisToBeReceived: 495000,
+            feeInSatoshisToBePaid: 5000,
+            status: PegoutStatus.RECEIVED,
+            btcRawTransaction: 'testBtcRawTx',
+        });
+        expect(pegoutStatus).to.be.deepEqual(expectedResponse);
+    });
+    it('should return a Pegout Status: REJECTED', async () => {
+        getLastByOriginatingRskTxHash
+            .withArgs('RskTestTxId')
+            .resolves({
+                rskTxHash: 'RskTestTxId',
+                rskSenderAddress: undefined,
+                btcRecipientAddress: undefined,
+                valueInWeisSentToTheBridge: 0,
+                valueInSatoshisToBeReceived: 0,
+                feeInSatoshisToBePaid: 0,
+                status: PegoutStatus.REJECTED,
+                btcRawTransaction: undefined,
+                originatingRskTxHash: 'RskTestTxId',
+                rskBlockHeight: 0,
+                lastUpdatedOn: undefined,
+            });
+        const pegoutStatus = await pegoutStatusService.getPegoutStatusByRskTxHash('RskTestTxId');
+        const expectedResponse = new PegoutStatusDataModel({
+            status: PegoutStatus.REJECTED,
+        });
+        expect(pegoutStatus).to.be.deepEqual(expectedResponse);
+    });
+    it('should return a Pegout Status: WAITING_FOR_CONFIRMATION', async () => {
+        getLastByOriginatingRskTxHash
+            .withArgs('RskTestTxId')
+            .resolves({
+                rskTxHash: 'RskTestTxId',
+                rskSenderAddress: 'testSenderAddress',
+                btcRecipientAddress: 'testBtcRecipientAddress',
+                valueInWeisSentToTheBridge: 5000000000000000,
+                valueInSatoshisToBeReceived: 495000,
+                feeInSatoshisToBePaid: 5000,
+                status: PegoutStatus.WAITING_FOR_CONFIRMATION,
+                btcRawTransaction: 'testBtcRawTx',
+                originatingRskTxHash: 'RskTestTxId',
+                rskBlockHeight: 3158962,
+                lastUpdatedOn: Date.now(),
+            });
+        const pegoutStatus = await pegoutStatusService.getPegoutStatusByRskTxHash('RskTestTxId');
+        const expectedResponse = new PegoutStatusDataModel({
+            rskTxHash: 'RskTestTxId',
+            rskSenderAddress: 'testSenderAddress',
+            btcRecipientAddress: 'testBtcRecipientAddress',
+            valueInWeisSentToTheBridge: 5000000000000000,
+            valueInSatoshisToBeReceived: 495000,
+            feeInSatoshisToBePaid: 5000,
+            status: PegoutStatus.WAITING_FOR_CONFIRMATION,
+            btcRawTransaction: 'testBtcRawTx',
+        });
+        expect(pegoutStatus).to.be.deepEqual(expectedResponse);
+    });
+    it('should return a Pegout Status: WAITING_FOR_SIGNATURE', async () => {
+        getLastByOriginatingRskTxHash
+            .withArgs('RskTestTxId')
+            .resolves({
+                rskTxHash: 'RskTestTxId',
+                rskSenderAddress: 'testSenderAddress',
+                btcRecipientAddress: 'testBtcRecipientAddress',
+                valueInWeisSentToTheBridge: 5000000000000000,
+                valueInSatoshisToBeReceived: 495000,
+                feeInSatoshisToBePaid: 5000,
+                status: PegoutStatus.WAITING_FOR_SIGNATURE,
+                btcRawTransaction: 'testBtcRawTx',
+                originatingRskTxHash: 'RskTestTxId',
+                rskBlockHeight: 3158962,
+                lastUpdatedOn: Date.now(),
+            });
+        const pegoutStatus = await pegoutStatusService.getPegoutStatusByRskTxHash('RskTestTxId');
+        const expectedResponse = new PegoutStatusDataModel({
+            rskTxHash: 'RskTestTxId',
+            rskSenderAddress: 'testSenderAddress',
+            btcRecipientAddress: 'testBtcRecipientAddress',
+            valueInWeisSentToTheBridge: 5000000000000000,
+            valueInSatoshisToBeReceived: 495000,
+            feeInSatoshisToBePaid: 5000,
+            status: PegoutStatus.WAITING_FOR_SIGNATURE,
+            btcRawTransaction: 'testBtcRawTx',
+        });
+        expect(pegoutStatus).to.be.deepEqual(expectedResponse);
+    });
+    it('should return a Pegout Status: SIGNED', async () => {
+        getLastByOriginatingRskTxHash
+            .withArgs('RskTestTxId')
+            .resolves({
+                rskTxHash: 'RskTestTxId',
+                rskSenderAddress: 'testSenderAddress',
+                btcRecipientAddress: 'testBtcRecipientAddress',
+                valueInWeisSentToTheBridge: 5000000000000000,
+                valueInSatoshisToBeReceived: 495000,
+                feeInSatoshisToBePaid: 5000,
+                status: PegoutStatus.SIGNED,
+                btcRawTransaction: 'testBtcRawTx',
+                originatingRskTxHash: 'RskTestTxId',
+                rskBlockHeight: 3158962,
+                lastUpdatedOn: Date.now(),
+            });
+        const pegoutStatus = await pegoutStatusService.getPegoutStatusByRskTxHash('RskTestTxId');
+        const expectedResponse = new PegoutStatusDataModel({
+            rskTxHash: 'RskTestTxId',
+            rskSenderAddress: 'testSenderAddress',
+            btcRecipientAddress: 'testBtcRecipientAddress',
+            valueInWeisSentToTheBridge: 5000000000000000,
+            valueInSatoshisToBeReceived: 495000,
+            feeInSatoshisToBePaid: 5000,
+            status: PegoutStatus.SIGNED,
+            btcRawTransaction: 'testBtcRawTx',
+        });
+        expect(pegoutStatus).to.be.deepEqual(expectedResponse);
+    });
+    it('should return an error if the database has an exception', async () => {
+        getLastByOriginatingRskTxHash
+            .withArgs('RskTestTxId')
+            .rejects(new Error('Unexpected Database error'));
+        return expect(pegoutStatusService.getPegoutStatusByRskTxHash('RskTestTxId'))
+            .to.be.rejectedWith(new Error('Unexpected Database error'));
+    });
+});
