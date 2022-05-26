0c736cf061496cab4cd6c0d6cf1b4b4163d42fc5
Ronald Sarmiento <roasarmientoga@unal.edu.co>
2022-05-26 01:53:33 -0500

    Using Pegout status service on TxStatus Controller
                    I've updated the TxStatus controller in order to ask for
                    pegoutStatus when there are no pegin status available

--- src/controllers/tx-status.controller.ts
+++ src/controllers/tx-status.controller.ts
@@ -4,7 +4,8 @@ import {PeginStatusError} from "../models/pegin-status-error.model";
 import {getLogger, Logger} from "log4js";
 import {inject} from "@loopback/core";
 import {ServicesBindings} from "../dependency-injection-bindings";
-import {PeginStatusService} from "../services";
+import {PeginStatusService, PegoutStatusService} from "../services";
+import {PegoutStatus} from "../models/rsk/pegout-status-data-model";
 
 export class TxStatusController {
   private logger: Logger;
@@ -12,8 +13,8 @@ export class TxStatusController {
   constructor(
       @inject(ServicesBindings.PEGIN_STATUS_SERVICE)
       protected peginStatusService: PeginStatusService,
-      // @inject(ServicesBindings.PEGOUT_STATUS_SERVICE)
-      // protected pegoutStatusService: PegoutStatusService
+      @inject(ServicesBindings.PEGOUT_STATUS_SERVICE)
+      protected pegoutStatusService: PegoutStatusService
   ) {
     this.logger = getLogger('TxStatusController');
   }
@@ -34,7 +35,11 @@ export class TxStatusController {
     try {
       this.logger.debug(`[getTxStatus] Started with txId ${txId}`);
       const peginStatus = await this.getPeginStatus(txId);
-      if (peginStatus.status !== Status.ERROR_NOT_A_PEGIN && peginStatus.status !== Status.ERROR_UNEXPECTED) {
+      if (
+          peginStatus.status !== Status.ERROR_NOT_A_PEGIN
+          && peginStatus.status !== Status.ERROR_UNEXPECTED
+          && peginStatus.status !== Status.NOT_IN_BTC_YET
+      ) {
         this.logger.debug(`[getTxStatus] Pegin status got for txId ${txId} - Status:${peginStatus.status}`);
         txStatus = new TxStatus({
           type: TxStatusType.PEGIN,
@@ -42,6 +47,16 @@ export class TxStatusController {
         });
         return txStatus;
       }
+      const pegoutStatus = await this.pegoutStatusService.getPegoutStatusByRskTxHash(txId);
+      if (pegoutStatus.status !== PegoutStatus.NOT_FOUND) {
+        this.logger.debug(`[getTxStatus] Pegout status got for txId ${txId} - Status:${pegoutStatus.status}`);
+        txStatus = new TxStatus({
+          type: TxStatusType.PEGOUT,
+          txDetails: pegoutStatus,
+        });
+        return txStatus;
+      }
+      this.logger.warn(`[getTxStatus] Tx id not found: ${txId}`);
       txStatus = new TxStatus({
         type: TxStatusType.INVALID_DATA,
       });
@@ -55,7 +70,7 @@ export class TxStatusController {
     }
   }
 
-  async getPeginStatus(txId: string): Promise<PeginStatus> {
+  private async getPeginStatus(txId: string): Promise<PeginStatus> {
     try {
       const result = await this.peginStatusService.getPeginSatusInfo(txId);
       this.logger.debug(`[getPeginStatus] Found tx with status ${result.status}`);
