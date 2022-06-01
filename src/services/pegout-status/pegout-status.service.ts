7618643a899f638e5b50aa005bead722aa6e6ff0
Alex Braz <alexjavabraz@gmail.com>
2022-06-01 16:52:07 -0300

    add logic to obtain pegoutstatus

--- src/services/pegout-status/pegout-status.service.ts
+++ src/services/pegout-status/pegout-status.service.ts
@@ -3,19 +3,26 @@ import {inject} from "@loopback/core";
 import {ServicesBindings} from "../../dependency-injection-bindings";
 import {PegoutStatus, PegoutStatusAppDataModel} from "../../models/rsk/pegout-status-data-model";
 import {PegoutStatusDataService} from "../pegout-status-data-services/pegout-status-data.service";
+import {RskNodeService} from "../rsk-node.service";
+import Web3 from 'web3';
 
 export class PegoutStatusService {
     private logger: Logger;
     private pegoutStatusDataService: PegoutStatusDataService;
+    private web3: Web3;
+    private rskNodeService:RskNodeService;
 
     constructor(
         @inject(ServicesBindings.PEGOUT_STATUS_DATA_SERVICE)
             pegoutStatusDataService: PegoutStatusDataService,
+        @inject(ServicesBindings.RSK_NODE_SERVICE)
+            rskNodeService: RskNodeService
     ) {
         this.logger = getLogger('pegoutStatusService');
         this.pegoutStatusDataService = pegoutStatusDataService;
+        this.rskNodeService = rskNodeService;
+        this.web3 = new Web3(`${process.env.RSK_NODE_HOST}`);
     }
-
 
     public getPegoutStatusByRskTxHash(rskTxHash: string): Promise<PegoutStatusAppDataModel> {
         return new Promise<PegoutStatusAppDataModel>((resolve, reject) => {
@@ -23,7 +30,15 @@ export class PegoutStatusService {
             this.pegoutStatusDataService.getLastByOriginatingRskTxHash(rskTxHash)
                 .then((pegoutStatusDbDataModel) => {
                     if (!pegoutStatusDbDataModel) {
-                        pegoutStatus.status = PegoutStatus.NOT_FOUND;
+
+                        let transaction = this.rskNodeService.getTransactionReceipt(rskTxHash);
+                        
+                        if(transaction) {
+                            pegoutStatus = processTransaction(transaction);
+                        } else {
+                            pegoutStatus.status = PegoutStatus.NOT_FOUND;
+                        }
+
                     } else if (pegoutStatusDbDataModel.status === PegoutStatus.REJECTED) {
                         pegoutStatus.status = PegoutStatus.REJECTED;
                     } else {
@@ -39,3 +54,9 @@ export class PegoutStatusService {
         });
     }
 }
+function processTransaction(transaction: Promise<any>): PegoutStatusAppDataModel {
+    let pegoutStatus: PegoutStatusAppDataModel = new PegoutStatusAppDataModel();
+    pegoutStatus.status = PegoutStatus.CREATED;
+    return pegoutStatus;
+}
+
