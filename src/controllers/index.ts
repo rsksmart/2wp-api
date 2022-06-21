37816dab7396593c0bdad35ae488f0f2959d81c7
jeremy-then <jeremy.then@iovlabs.org>
2022-06-21 10:57:46 -0400

    Refactors getTxStatus to try to get a pegout in case trying to get a pegin fails

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,6 @@
 export * from './balance.controller';
 export * from './broadcast.controller';
 export * from './pegin-configuration.controller';
-export * from './pegin-status.controller';
 export * from './pegin-tx.controller';
 export * from './tx-fee.controller';
 export * from './tx.controller';
