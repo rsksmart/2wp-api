9a605758847ee06fa9abeb947a7fb02dd632cf4e
Alex Braz <alexjavabraz@gmail.com>
2022-11-07 14:43:14 -0300

    avoid error 500 when only one address fail

--- src/services/unused-address.service.ts
+++ src/services/unused-address.service.ts
@@ -25,24 +25,34 @@ export class UnusedAddressService {
     const addressInfoPromises = [];
     for (const address of addresses) {
        addressInfoPromises.push(this.bitcoinService.getAddressInfo(address));
-    }
-    return Promise.all(addressInfoPromises)
-    .then((bitcoinAddressList: BitcoinAddress[]) => {
-      for (const bitcoinAddress of bitcoinAddressList) {
-        const totalReceived: SatoshiBig = new SatoshiBig(bitcoinAddress.totalReceived, 'satoshi');
-        const totalSent: SatoshiBig = new SatoshiBig(bitcoinAddress.totalSent, 'satoshi');
-        const unused = totalReceived.eq(0) && totalSent.eq(0);
-        response.data.push(new AddressUsedStatus({
-          address: bitcoinAddress.address,
-          unused,
-        }));
-        this.logger.trace(`[isUnusedAddresses] ${bitcoinAddress} is ${unused ? 'unused' : 'used'}`);
+       public async isUnusedAddresses(addresses: string[]): Promise<UnusedAddressResponse> {
+        this.logger.debug("[isUnusedAddresses] starting to analyse addresses");
+        const response: UnusedAddressResponse =  new UnusedAddressResponse({ data: [] });
+        const addressInfoPromises = [];
+        for (const address of addresses) {
+          try {
+           addressInfoPromises.push(await this.bitcoinService.getAddressInfo(address));
+          } catch(e) {
+            this.logger.debug(`[isUnusedAddresses] Error obtaining one address, keep trying to get everyone else. ${e}`);
+          }
+        }
+        return Promise.all(addressInfoPromises)
+        .then((bitcoinAddressList: BitcoinAddress[]) => {
+          for (const bitcoinAddress of bitcoinAddressList) {
+            const totalReceived: SatoshiBig = new SatoshiBig(bitcoinAddress.totalReceived, 'satoshi');
+            const totalSent: SatoshiBig = new SatoshiBig(bitcoinAddress.totalSent, 'satoshi');
+            const unused = totalReceived.eq(0) && totalSent.eq(0);
+            response.data.push(new AddressUsedStatus({
+              address: bitcoinAddress.address,
+              unused,
+            }));
+            this.logger.trace(`[isUnusedAddresses] ${bitcoinAddress} is ${unused ? 'unused' : 'used'}`);
+          }
+          return response;
+        })
+        .catch((e: unknown) => {
+           this.logger.debug(`[isUnusedAddresses] Failed. ${e}`);
+           throw e;
+         });
       }
-      return response;
-    })
-    .catch((e: unknown) => {
-       this.logger.debug(`[isUnusedAddresses] Failed. ${e}`);
-       throw e;
-     });
-  }
 }
