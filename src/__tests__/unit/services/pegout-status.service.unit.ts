a29a5b1d92da61ee282fcbf6e493e80286526d5f
julia zack <julia.zack@iovlabs.org>
2023-05-16 16:22:50 -0300

    Improve pegout service & bridge service tests

--- src/__tests__/unit/services/pegout-status.service.unit.ts
+++ src/__tests__/unit/services/pegout-status.service.unit.ts
@@ -7,18 +7,29 @@ import {
     PegoutStatusAppDataModel,
 } from "../../../models/rsk/pegout-status-data-model";
 import { RskNodeService } from '../../../services/rsk-node.service';
+import { RskTransaction } from '../../../models/rsk/rsk-transaction.model';
+import { ExtendedBridgeTxModel } from '../../../services/extended-bridge-tx';
+import { BridgeEvent, BridgeMethod, Transaction } from 'bridge-transaction-parser';
 
 describe('Pegout Status Service:', () => {
     let pegoutStatusService: PegoutStatusService;
+    let pegoutStatusServiceMocked: PegoutStatusService;
     let getLastByOriginatingRskTxHash: sinon.SinonStub;
     let rskNodeService: RskNodeService;
+    let getTransaction: sinon.SinonStub;
+    let getBridgeTransaction: sinon.SinonStub;
+    let processTransaction: sinon.SinonStub;
     let pegoutStatusDataService: StubbedInstanceWithSinonAccessor<PegoutStatusMongoDbDataService>;
     beforeEach(() => {
         pegoutStatusDataService = createStubInstance(PegoutStatusMongoDbDataService);
+        pegoutStatusServiceMocked = createStubInstance(PegoutStatusService);
         rskNodeService = createStubInstance(RskNodeService);
-        getLastByOriginatingRskTxHash = pegoutStatusDataService.getLastByOriginatingRskTxHashNewest as sinon.SinonStub;
-
         pegoutStatusService = new PegoutStatusService(pegoutStatusDataService, rskNodeService);
+        getTransaction = rskNodeService.getTransaction as sinon.SinonStub;
+        getBridgeTransaction = rskNodeService.getBridgeTransaction as sinon.SinonStub;
+        getLastByOriginatingRskTxHash = pegoutStatusDataService.getLastByOriginatingRskTxHashNewest as sinon.SinonStub;
+        //@ts-ignore
+        processTransaction = pegoutStatusServiceMocked.processTransaction as sinon.SinonStub;
     });
     it('should return a valid Pegout Status if there is no record on database', async () => {
         getLastByOriginatingRskTxHash
@@ -59,6 +70,61 @@ describe('Pegout Status Service:', () => {
             btcRawTransaction: 'testBtcRawTx',
         });
         expect(pegoutStatus).to.be.deepEqual(expectedResponse);
+    });
+    it('should return a Pegout Status: PENDING', async () => {
+        let rskTransactionWithoutReceipt: RskTransaction = { hash: 'txHash', blockHash: 'blockHash', blockHeight: 300000, data: 'data', createdOn: new Date(), to: 'to', receipt: null };
+        getTransaction
+            .withArgs('RskTestTxId')
+            .resolves(rskTransactionWithoutReceipt);
+        getLastByOriginatingRskTxHash
+            .withArgs('RskTestTxId')
+            .resolves(null);
+        const pegoutStatus = await pegoutStatusService.getPegoutStatusByRskTxHash('RskTestTxId');
+        expect(pegoutStatus.status).to.be.deepEqual(PegoutStatus.PENDING);
+    });
+    it('should return a Pegout Status: != PENDING', async () => {
+        const log = [{ address: '', data: '', topics: ['',''], logIndex: 0, transactionIndex: 0, transactionHash: '', blockHash: '', blockNumber: 0 },
+                        { address: '', data: '', topics: ['',''], logIndex: 0, transactionIndex: 0, transactionHash: '', blockHash: '', blockNumber: 0 }];
+        const txReceipt = { status: true, transactionHash: '', transactionIndex: 0, blockHash: '', blockNumber: 0, from: '', to: '', cumulativeGasUsed: 0, gasUsed: 0, logs: log, logsBloom: ''};
+        const rskTransactionWithReceipt: RskTransaction = { hash: 'txHash', blockHash: 'blockHash', blockHeight: 300000, data: 'data', createdOn: new Date(), to: 'to', receipt: txReceipt};
+        
+        const bridgeMethod: BridgeMethod = { name: '', signature: '', arguments: {} };
+        const bridgeEvent: BridgeEvent[] = [ { name: '', signature: '', arguments: {} },
+                                            { name: '', signature: '', arguments: {} } ];
+        const transaction: Transaction = { txHash: '', method: bridgeMethod, events: bridgeEvent, blockNumber: 0 }
+        
+        getLastByOriginatingRskTxHash
+            .withArgs('RskTestTxId')
+            .resolves(null);
+        
+        getTransaction
+            .withArgs('RskTestTxId')
+            .resolves(rskTransactionWithReceipt);
+        
+        getBridgeTransaction
+            .withArgs('RskTestTxId')
+            .resolves(transaction)
+        
+        const extendedModel: ExtendedBridgeTxModel = new ExtendedBridgeTxModel(transaction, rskTransactionWithReceipt)
+        
+        const processedTransaction = new PegoutStatusAppDataModel({
+            originatingRskTxHash: 'RskTestTxId',
+            rskTxHash: 'RskTestTxId',
+            rskSenderAddress: 'testSenderAddress',
+            btcRecipientAddress: 'testBtcRecipientAddress',
+            valueRequestedInSatoshis: 500000,
+            valueInSatoshisToBeReceived: 495000,
+            feeInSatoshisToBePaid: 5000,
+            status: PegoutStatus.RECEIVED,
+            btcRawTransaction: 'testBtcRawTx',
+        });
+        
+        processTransaction
+            .withArgs(extendedModel)
+            .resolves(processedTransaction);
+
+        const pegoutStatus = await pegoutStatusService.getPegoutStatusByRskTxHash('RskTestTxId');
+        expect(pegoutStatus.status).to.not.be.deepEqual(PegoutStatus.PENDING);
     });
     it('should return a Pegout Status: REJECTED', async () => {
         getLastByOriginatingRskTxHash
