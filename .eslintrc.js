75c21b5efd890287e2e4c4dda13658dcde635d2c
lserra-iov <lucio.serra@iovlabs.org>
2024-01-11 09:47:08 -0300

    Update eslint config
    - Extend airbnb configs.
    - Change some rules severity to progressively fix problems.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: '@loopback/eslint-config',
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "@loopback/eslint-config",
+  ],
+  rules: {
+    "import/prefer-default-export": "warn",
+    "no-await-in-loop": "warn",
+    "class-methods-use-this": "warn",
+    "prefer-destructuring": "warn",
+    "@typescript-eslint/lines-between-class-members": "off",
+    "no-plusplus": "off",
+  }
 };
