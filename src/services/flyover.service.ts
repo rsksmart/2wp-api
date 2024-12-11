1c8e2fc4ca09920e600399e3c18f3dc85d3257ce
lserra-iov <lucio.serra@iovlabs.org>
2024-12-11 11:42:03 -0300

    Store quote's bigint values with appropriate type

--- src/services/flyover.service.ts
+++ src/services/flyover.service.ts
@@ -7,6 +7,29 @@ import * as constants from '../constants';
 
 interface FlyoverStatusMongoModel extends mongoose.Document, FlyoverStatusModel {}
 
+const quoteSchema = new Schema<QuoteDbModel>({
+  agreementTimestamp: {type: Number},
+  gasFeeOnWei: {type: Schema.Types.BigInt},
+  nonce: {type: Schema.Types.BigInt},
+  penaltyFeeOnWei: {type: Schema.Types.BigInt},
+  btcRefundAddress: {type: String},
+  lbcAddress: {type: String},
+  lpBtcAddress: {type: String},
+  rskRefundAddress: {type: String},
+  liquidityProviderRskAddress: {type: String},
+  callFeeOnSatoshi: {type: Schema.Types.BigInt},
+  callOnRegister: {type: Boolean},
+  confirmations: {type: Number},
+  contractAddr: {type: String},
+  data: {type: String},
+  fedBTCAddr: {type: String},
+  gasLimit: {type: Schema.Types.BigInt},
+  lpCallTime: {type: Number},
+  productFeeAmountOnSatoshi: {type: Schema.Types.BigInt},
+  timeForDepositInSeconds: {type: Number},
+  valueOnSatoshi: {type: Schema.Types.BigInt},
+})
+
 const FlyoverStatusSchema = new mongoose.Schema({
   txHash: {type: String, required: true},
   date: {type: Date, required: true},
@@ -17,7 +40,7 @@ const FlyoverStatusSchema = new mongoose.Schema({
   senderAddress: {type: String, required: true},
   recipientAddress: {type: String, required: true},
   quoteHash: {type: String, required: true},
-  quote: Schema.Types.Mixed,
+  quote: quoteSchema,
   acceptedQuoteSignature: {type: String},
 });
 
@@ -90,7 +113,7 @@ export class FlyoverService extends MongoDbDataService<FlyoverStatusModel, Flyov
     flyoverStatus.recipientAddress = payload?.details?.recipientAddress ?? '';
     flyoverStatus.blockToBeFinished = currentBlock + Number(payload?.details?.blocksToCompleteTransaction ?? 0);
     flyoverStatus.quoteHash = payload?.quoteHash ?? '';
-    flyoverStatus.quote = payload?.quote ?? {} as QuoteDbModel;
+    flyoverStatus.quote = { ...payload?.quote , ...(payload?.quote?.gasLimit && { gasLimit: BigInt(payload?.quote?.gasLimit) })} as QuoteDbModel;
     flyoverStatus.acceptedQuoteSignature = payload?.acceptedQuoteSignature ?? '';
     return this.set(flyoverStatus);
   }
