9bb2ae3ecd54be8150a438fb0828daf974fa9308
ronaldsg <ronaldsg20@gmail.com>
2025-05-27 10:43:22 -0500

    Include string and negative assertions for parse units functions

--- src/utils/parseUnits.ts
+++ src/utils/parseUnits.ts
@@ -1,10 +1,12 @@
 import { formatUnits } from 'ethers';
 
 export const stringWeiToDecimalString = (value: string) => {
-    if(value === '0') return '0';
+    if (Number.isNaN(value) || Number.isNaN(parseFloat(value))) return '0';
+    if(BigInt(value) <= 0) return '0';
     return formatUnits(BigInt(value), 18);
 };
 export const stringSatoshiToDecimalString = (value: string) => {
-    if(value === '0') return '0';
+    if (Number.isNaN(value) || Number.isNaN(parseFloat(value))) return '0';
+    if(BigInt(value) <= 0) return '0';
     return formatUnits(BigInt(value), 8);
 };
