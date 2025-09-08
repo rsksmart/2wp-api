300e4a1ec882a2a4785603b74acbadfe4ee69806
ronaldsg <ronaldsg20@gmail.com>
2025-09-08 10:43:38 -0500

    Assert model in the logs requests

--- src/models/log-entry.model.ts
+++ src/models/log-entry.model.ts
@@ -1,6 +1,25 @@
 import {Model, model, property} from '@loopback/repository';
 
 @model()
+export class LogError {
+  @property({
+    type: 'string',
+    required: true,
+    jsonSchema: {
+      minLength: 3,
+      maxLength: 30,
+      pattern: '^[a-zA-Z0-9_\\- ]+$',
+    },
+  })
+  message: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  code: number;
+}
+@model()
 export class LogEntry extends Model {
   @property({
     type: 'string',
@@ -17,11 +36,32 @@ export class LogEntry extends Model {
   @property({
     type: 'string',
     required: true,
+    jsonSchema: {
+      minLength: 3,
+      maxLength: 50,
+      pattern: '^[a-zA-Z0-9_\\- ]+$',
+    },
   })
   location: string;
 
   @property({
     type: 'object',
+    jsonSchema: {
+      type: 'object',
+      properties: {
+        message: {
+          type: 'string',
+          minLength: 3,
+          maxLength: 30,
+          pattern: '^[a-zA-Z0-9_\\- ]+$',
+        },
+        code: {
+          type: 'number',
+        },
+      },
+      required: ['message', 'code'],
+      additionalProperties: false,
+    },
   })
-  error?: Error;
+  error?: LogError;
 }
